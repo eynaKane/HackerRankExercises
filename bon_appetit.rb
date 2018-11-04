03b3691a36971e67529dde3d433a5cc6706915bc
Eyna Kane <eyna.kane@gmail.com>
2018-11-04 13:08:00 -0500

    fix some rubocop errors

--- bon_appetit.rb
+++ bon_appetit.rb
@@ -1,23 +1,19 @@
-#!/bin/ruby
-
+# https://www.hackerrank.com/challenges/bon-appetit/problem
 require 'json'
 require 'stringio'
 
-# Complete the bonAppetit function below.
-def bonAppetit(bill, k, b)
-	bill.delete_at(k)
-	split = bill.sum / 2
+def bon_appetit(bill, k, b)
+  bill.delete_at(k)
+  split = bill.sum / 2
 
-	if split == b
-		puts "Bon Appetit"
-	else
-		puts "#{b - split}"
-	end
+  if split == b
+    puts 'Bon Appetit'
+  else
+    puts b - split
+  end
 end
 
 nk = gets.rstrip.split
-
-n = nk[0].to_i
 
 k = nk[1].to_i
 
@@ -25,18 +21,18 @@ bill = gets.rstrip.split.map(&:to_i)
 
 b = gets.strip.to_i
 
-bonAppetit bill, k, b
+bon_appetit bill, k, b
 
-# ❯ ruby bon_appetit.rb 
+# ruby bon_appetit.rb
 # 4 1
 # 3 10 2 9
 # 7
-# ❯
+
 # Bon Appetit
 
-# ❯ ruby bon_appetit.rb
+# ruby bon_appetit.rb
 # 4 1
 # 3 10 2 9
 # 12
-# ❯
+
 # 5
