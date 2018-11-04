8e332ae02153eaa5d7b425c0f45f628fd1595245
Eyna Kane <eyna.kane@gmail.com>
2018-11-04 12:58:40 -0500

    fix some rubocop errors

--- birthday_cake.rb
+++ birthday_cake.rb
@@ -1,23 +1,16 @@
-#!/bin/ruby
-
+# https://www.hackerrank.com/challenges/the-birthday-bar/problem
 require 'json'
 require 'stringio'
 require 'pry'
 
-# Complete the birthday function below.
 def birthday(s, d, m, n)
-  # 0 1 2 3 4
-  # 1 2 1 3 2
-  # 3 2
-  # 2
-
   return 0 if m > n
 
   slice_ctr = 0
   ctr = 0
 
   while ctr < n
-    slice_sum = s[ctr..ctr+(m-1)].sum
+    slice_sum = s[ctr..ctr + (m - 1)].sum
     slice_ctr += 1 if slice_sum == d
     ctr += 1
   end
@@ -39,9 +32,9 @@ result = birthday s, d, m, n
 
 puts result
 
-# ❯ ruby birthday_cake.rb 
+# ruby birthday_cake.rb
 # 6
 # 1 1 1 1 1 1
 # 3 2
-# ❯
+
 # 0
