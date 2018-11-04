7bf55f7adce69fecbadc24a649f83f62b553f922
Eyna Kane <ekane@optoro.com>
2018-11-03 23:11:43 -0400

    return an integer array with number of times record was broken

---
+++ best_worst_records.rb
@@ -0,0 +1,29 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+require 'pry'
+
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores)
+	high = low = scores.first
+	max = min = 0
+
+	scores.map do |score|
+		if score > high
+			high = score
+			max += 1
+		elsif score < low
+			low = score
+			min += 1
+		end
+	end
+	[max, min]
+end
+
+scores = gets.rstrip.split(' ').map(&:to_i)
+
+result = breakingRecords scores
+
+puts result.join " "
