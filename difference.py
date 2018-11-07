7af368b55eab7b33220b401b72ec1a0a5e3242cf
Eyna Kane <eyna.kane@gmail.com>
2018-11-07 09:16:38 -0500

    find maximum absolute difference between any 2 numbers in an array

---
+++ difference.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/30-scope/problem
+class Difference:
+  def __init__(self, a):
+    self.__elements = a
+
+  def computeDifference(self):
+    d._Difference__elements.sort()
+    sorted_elements = d._Difference__elements
+    self.maximumDifference = sorted_elements[-1] - sorted_elements[0]
+
+_ = raw_input()
+
+a = [int(e) for e in raw_input().split(' ')]
+
+d = Difference(a)
+
+d.computeDifference()
+
+print d.maximumDifference
+
+# python difference.py  
+# 3
+# 1 2 5
+
+# 4
+
+# python difference.py
+# 5
+# 8 19 3 2 7
+
+# 17
