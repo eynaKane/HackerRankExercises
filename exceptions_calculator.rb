4156346d352ce34f42cef69fa49bda8a5032ef28
Eyna Kane <eyna.kane@gmail.com>
2018-11-10 13:31:02 -0500

    throw an exception when input is negative

---
+++ exceptions_calculator.rb
@@ -0,0 +1,43 @@
+# https://www.hackerrank.com/challenges/30-more-exceptions/problem
+require 'pry'
+
+# Exception class
+class RangeError < StandardError
+end
+
+# Calculator class
+class Calculator
+  def power(n, p)
+    raise RangeError, 'n and p should be non-negative' if n < 0 || p < 0
+
+    n**p
+  end
+end
+
+my_calculator = Calculator.new
+
+t = gets.to_i
+
+(1..t).each do
+  n, p = gets.split.map(&:to_i)
+  begin
+    ans = my_calculator.power(n, p)
+    puts ans.to_i
+  rescue RangeError => e
+    puts e.message
+  end
+end
+
+# ruby exceptions_calculator.rb
+# 4
+# 3 5
+# 243
+
+# 2 4
+# 16
+
+# -1 -2
+# n and p should be non-negative
+
+# -1 3
+# n and p should be non-negative
