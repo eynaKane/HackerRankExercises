39c376bf5538dbe2e23e90d594d48fa218db9727
Eyna Kane <eyna.kane@gmail.com>
2018-11-15 22:41:20 -0500

    return the height of the binary search tree

---
+++ binary_search_tree.rb
@@ -0,0 +1,69 @@
+# https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+require 'pry'
+
+# Node class
+class Node
+  attr_accessor :left, :right, :data
+
+  def initialize(data)
+    @left = @right = nil
+    @data = data
+  end
+end
+
+# Solution class
+class Solution
+  def insert(root, data)
+    return Node.new(data) if root.nil?
+
+    if data <= root.data
+      cur = insert(root.left, data)
+      root.left = cur
+    else
+      cur = insert(root.right, data)
+      root.right = cur
+    end
+
+    root
+  end
+
+  def get_height(root)
+    return -1 if root.nil?
+
+    left_depth = get_height(root.left)
+    right_depth = get_height(root.right)
+
+    if left_depth > right_depth
+      left_depth + 1
+    else
+      right_depth + 1
+    end
+  end
+end
+
+my_tree = Solution.new
+
+root = nil
+
+T = gets.to_i
+
+T.times do
+  data = gets.to_i
+  root = my_tree.insert(root, data)
+end
+
+height = my_tree.get_height(root)
+
+puts "The height of Binary Search Tree is #{height}"
+
+# ruby binary_search_tree.rb
+# 7
+# 3
+# 5
+# 2
+# 1
+# 4
+# 6
+# 7
+
+# The height of Binary Search Tree is 3
