861cbae6b55635fdd7dd83345a627bc357a952cd
Eyna Kane <eyna.kane@gmail.com>
2018-11-24 11:23:01 -0500

    add day5 challenge - loops

---
+++ 30DaysOfCode/day5_multiples_of_ten.rb
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/30-loops/problem
+require 'json'
+require 'stringio'
+
+def multiples(n)
+  ctr = 1
+  while ctr <= 10
+    puts "#{n} x #{ctr} = #{n * ctr}"
+    ctr += 1
+  end
+end
+
+n = gets.to_i
+
+multiples(n)
+
+# ruby day5_multiples_of_ten.rb
+# 2
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10 = 20
