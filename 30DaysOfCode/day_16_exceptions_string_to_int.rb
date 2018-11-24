62191a6efd0c465838eb54dd973fd99ce494fc94
Eyna Kane <eyna.kane@gmail.com>
2018-11-24 13:04:50 -0500

    add day16 challenge - exceptions string to int

--- 30DaysOfCode/day_16_exceptions_string_to_int.rb
+++ 30DaysOfCode/day_16_exceptions_string_to_int.rb
@@ -9,12 +9,12 @@ rescue ArgumentError
   puts 'Bad String'
 end
 
-# ruby 30DaysOfCode/day_16_exceptions_string_to_int.rb
+# ruby day_16_exceptions_string_to_int.rb
 # 122
 
 # 122
 
-# ruby 30DaysOfCode/day_16_exceptions_string_to_int.rb
+# ruby day_16_exceptions_string_to_int.rb
 # abc
 
 # Bad String
