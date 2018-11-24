7b3a12a61f95366d441479c5bcc0a2d957e4f141
Eyna Kane <eyna.kane@gmail.com>
2018-11-24 11:03:10 -0500

    add day2 challenge - operators

---
+++ 30DaysOfCode/day2_meal.rb
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/30-operators/problem
+require 'json'
+require 'stringio'
+
+def solve(meal_cost, tip_percent, tax_percent)
+  additional_charges = (tip_percent + tax_percent) / 100.0
+  meal_cost += (meal_cost * additional_charges)
+  puts meal_cost.round
+end
+
+meal_cost = gets.to_f
+
+tip_percent = gets.to_i
+
+tax_percent = gets.to_i
+
+solve(meal_cost, tip_percent, tax_percent)
+
+# ruby day2_meal.rb
+# 12.00
+# 20
+# 8
+
+# 15
