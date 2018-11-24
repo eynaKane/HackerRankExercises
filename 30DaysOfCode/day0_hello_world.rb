3cf8254d9fa5b68c5efbbf5951b25577525514d5
Eyna Kane <eyna.kane@gmail.com>
2018-11-24 10:52:18 -0500

    create 30DaysOfCode folder

---
+++ 30DaysOfCode/day0_hello_world.rb
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/30-hello-world/problem
+input_string = gets
+
+puts "Hello, World.\n#{input_string}"
+
+# ruby day0_hello_world.rb
+# Welcome to 30 Days of Code!
+
+# Hello, World.
+# Welcome to 30 Days of Code!
