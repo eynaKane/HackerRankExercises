2ffefdc484a0bcf5a60ac3a4cf40af3b1b36f936
Eyna Kane <eyna.kane@gmail.com>
2018-11-24 11:29:31 -0500

    add day6 challenge - review loops

---
+++ 30DaysOfCode/day6_odd_or_even.rb
@@ -0,0 +1,34 @@
+# https://www.hackerrank.com/challenges/30-review-loop/problem
+def get_odd_even(value)
+  even = ''
+  odd = ''
+
+  value.split(//).each_with_index do |v, i|
+    if i.even?
+      even += v
+    else
+      odd += v
+    end
+  end
+
+  even + ' ' + odd
+end
+
+n = gets.to_i
+
+result = []
+
+n.times do |i|
+  value = gets.rstrip
+  result[i] = get_odd_even(value)
+end
+
+result.map { |r| puts r }
+
+# ruby day6_odd_or_even.rb
+# 2
+# Hacker
+# Rank
+
+# Hce akr
+# Rn ak
