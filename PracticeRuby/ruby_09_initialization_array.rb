dfd7fd2147d48d3f75804f65e5d9d5876414e200
Eyna Kane <eyna.kane@gmail.com>
2018-11-27 22:04:08 -0500

    using arrays - initialization

--- PracticeRuby/ruby_09_initialization_array.rb
+++ PracticeRuby/ruby_09_initialization_array.rb
@@ -1,38 +1,45 @@
 # https://www.hackerrank.com/challenges/ruby-array-initialization/problem
 require 'pry'
 
-array = []
-array1 = [nil]
-array2 = [10, 10]
-
 @_binding = binding
 def show_output(p, b = @_binding)
   # rubocop:disable Security/Eval
   eval("
     local_variables.each do |v|
       if eval(v.to_s + \".object_id\") == " + p.object_id.to_s + "
-        puts 'variable named ' + v.to_s + ' is defined!'
-        puts v.to_s + ' variable belongs to the class `'\
-          + eval(v.to_s).class.to_s + '`!'
-        puts v.to_s + ' is of size ' + eval(v.to_s).count.to_s + '!'
+        var = v.to_s
+        val = eval(var)
+        uniq_val = val.first ? val.first.to_s : 'nil'
+        puts 'variable named ' + var + ' is defined!'
+        puts var + ' variable belongs to the class `'\
+          + val.class.to_s + '`!'
+        puts var + ' is of size ' + val.size.to_s + '!'
+        puts 'Element of the ' + var + ' is ' + uniq_val\
+          + '!' unless val.size.zero?
       end
     end
   ", b)
   # rubocop:enable Security/Eval
 end
 
+array = []
+array1 = [nil]
+array2 = [10, 10]
+
 show_output(array)
 show_output(array1)
 show_output(array2)
 
+# ruby ruby_09_initialization_array.rb
+
 # variable named array is defined!
 # array variable belongs to the class `Array`!
-# array variable is of size 0!
-# variable named array_1 is defined!
-# array_1 variable belongs to the class `Array`!
-# array_1 variable is of size 1!
-# Element of the array_1 is nil!
-# variable named array_2 is defined!
-# array_1 variable belongs to the class `Array`!
-# array_2 variable is of size 2!
-# Each element of array_2 is 10!
+# array is of size 0!
+# variable named array1 is defined!
+# array1 variable belongs to the class `Array`!
+# array1 is of size 1!
+# Element of the array1 is nil!
+# variable named array2 is defined!
+# array2 variable belongs to the class `Array`!
+# array2 is of size 2!
+# Element of the array2 is 10!
